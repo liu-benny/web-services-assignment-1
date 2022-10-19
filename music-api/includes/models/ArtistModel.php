7fa1d1d46667d154ce3cb084cb1d3c746e17908d
Benny <82058278+liu-benny@users.noreply.github.com>
2022-10-18 20:30:04 -0400

    Added BaseModel.php, ArtistModel.php, artists_routes.php
    Data retrieval requests:
    1. Get all artists (resource URI: /artists), with filtering by name.
    2. Get the details of a given artists (resource URI: /artists/{artist_id}) .

---
+++ music-api/includes/models/ArtistModel.php
@@ -0,0 +1,57 @@
+<?php
+
+class ArtistModel extends BaseModel {
+
+    private $table_name = "artist";
+
+    /**
+     * A model class for the `artist` database table.
+     * It exposes operations that can be performed on artists records.
+     */
+    function __construct() {
+        // Call the parent class and initialize the database connection settings.
+        parent::__construct();
+    }
+
+    /**
+     * Retrieve all artists from the `artist` table.
+     * @return array A list of artists. 
+     */
+    public function getAll() {
+        $sql = "SELECT * FROM artist";
+        $data = $this->rows($sql);
+        return $data;
+    }
+
+    /**
+     * Get a list of artists whose name matches or contains the provided value.       
+     * @param string $artistName 
+     * @return array An array containing the matches found.
+     */
+    public function getWhereLike($artistName) {
+        $sql = "SELECT * FROM artist WHERE Name LIKE :name";
+        $data = $this->run($sql, [ ":name" => "%" . $artistName . "%"])->fetchAll();
+        return $data;
+    }
+
+    /**
+     * Retrieve an artist by its id.
+     * @param int $artist_id the id of the artist.
+     * @return array an array containing information about a given artist.
+     */
+    public function getArtistById($artist_id) {
+        $sql = "SELECT * FROM artist WHERE ArtistId = ?";
+        $data = $this->run($sql, [$artist_id])->fetch();
+        return $data;
+    }
+
+    public function createArtist($artists){
+        $artists = $this->insert("artist",$artists);
+        return $artists;
+    }
+
+    public function updateArtist($artists,$where){
+        $artists = $this->update("artist",$artists,$where);
+    }
+
+}
---
+++ music-api/includes/routes/artists_routes.php
@@ -0,0 +1,125 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+//var_dump($_SERVER["REQUEST_METHOD"]);
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . './../models/BaseModel.php';
+require_once __DIR__ . './../models/ArtistModel.php';
+
+// Callback for HTTP GET /artists
+//-- Supported filtering operation: by artist name.
+function handleGetAllArtists(Request $request, Response $response, array $args) {
+    $artists = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $artist_model = new ArtistModel();
+
+    // Step 1
+    // Retreive the query string parameter from the request's URI.
+    $filter_params = $request->getQueryParams();
+    if (isset($filter_params["name"])) {
+        // Fetch the list of artists matching the provided name.
+        $artists = $artist_model->getWhereLike($filter_params["name"]);
+    } else {
+        // No filtering by artist name detected.
+        $artists = $artist_model->getAll();
+    } 
+    // Step 2   
+    // Handle serve-side content negotiation and produce the requested representation.    
+    $requested_format = $request->getHeader('Accept');
+    //--
+    // Step 3
+    //-- We verify the requested resource representation.    
+    if ($requested_format[0] === APP_MEDIA_TYPE_JSON) {
+        $response_data = json_encode($artists, JSON_INVALID_UTF8_SUBSTITUTE);
+    } else {
+        $response_data = json_encode(getErrorUnsupportedFormat());
+        $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+    }
+    $response->getBody()->write($response_data);
+    return $response->withStatus($response_code);
+}
+
+// URI: /artists/{artist_id}
+function handleGetArtistById(Request $request, Response $response, array $args) {
+    $artist_info = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $artist_model = new ArtistModel();
+
+    // Retreive the artist from the request's URI.
+    $artist_id = $args["artist_id"];
+    if (isset($artist_id)) {
+        // Fetch the info about the specified artist.
+        $artist_info = $artist_model->getArtistById($artist_id);
+        if (!$artist_info) {
+            // No matches found?
+            $response_data = makeCustomJSONError("resourceNotFound", "No matching record was found for the specified artist.");
+            $response->getBody()->write($response_data);
+            return $response->withStatus(HTTP_NOT_FOUND);
+        }
+    }
+    // Handle serve-side content negotiation and produce the requested representation.    
+    $requested_format = $request->getHeader('Accept');
+    //--
+    //-- We verify the requested resource representation.    
+    if ($requested_format[0] === APP_MEDIA_TYPE_JSON) {
+        $response_data = json_encode($artist_info, JSON_INVALID_UTF8_SUBSTITUTE);
+    } else {
+        $response_data = json_encode(getErrorUnsupportedFormat());
+        $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+    }
+    $response->getBody()->write($response_data);
+    return $response->withStatus($response_code);
+}
+
+
+function handleCreateArtists(Request $request, Response $response, array $args){
+    
+    $artist_model = new ArtistModel();
+
+    $parsed_data = $request->getParsedBody();
+
+    $artists_id = "";
+    $artists_name = "";
+
+    foreach($parsed_data as $artist){
+
+        $artists_record = array("ArtistId" => $artist['ArtistId'] , "Name" => $artist['Name']);
+        $artist_model->createArtist($artists_record);
+    }
+
+
+    // for( $index = 0; $index < count($parsed_data); $index++){
+    //     $artist = $parsed_data[$index];
+
+    //     $artists_id .= $artist['ArtistId']. "-->";
+    //     $artists_name .= $artist['Name'] . "-->";
+
+    // }
+
+    // $parsed_body = 
+    $response->getBody()->write($artists_id . " ". $artists_name . " ");
+    return $response;
+}
+
+function handleUpdateArtist(Request $request, Response $response, array $args){
+    
+    $artist_model = new ArtistModel();
+
+    $parsed_data = $request->getParsedBody();
+
+    $artists_id = "";
+    $artists_name = "";
+
+    foreach($parsed_data as $artist){
+
+        $artists_record = array( "Name" => $artist['Name']);
+        $artist_model->updateArtist($artists_record, array("ArtistId" => $artist['ArtistId']));
+    }
+
+    // $response->getBody()->write($artists_id . " ". $artists_name . " ");
+    return $response;
+}
